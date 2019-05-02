c31b92715fe1bdaef7cd569cd892a66720434855
ledbagholberton <789@holbertonschool.com>
2019-05-02 04:21:47 +0000

    Point 5

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print("{} arguments.".format(len(sys.argv) - 1))
-else:
-    print("{} arguments.".format(len(sys.argv) - 1))
-    for i in range (1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments.".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sys
-suma = 0
-for i in range(1, len(sys.argv)):
-    suma = suma + int(sys.argv[i])
-print("{:d}".format(suma))
+if __name__ == "__main__":
+    import sys
+    suma = 0
+    for i in range(1, len(sys.argv)):
+        suma = suma + int(sys.argv[i])
+    print("{:d}".format(suma))
