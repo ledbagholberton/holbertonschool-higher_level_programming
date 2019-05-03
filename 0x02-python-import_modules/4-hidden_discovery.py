6629e8290769c10a08abb7960941611f1fc59d1c
ledbagholberton <789@holbertonschool.com>
2019-05-03 04:07:19 +0000

    Dir

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-for i in dir(hidden_4):
-    if i[0] is not "_":
-        print("{}".format(i))
+if __name__ is "__main__"
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[0] is not "_":
+            print("{}".format(i))
