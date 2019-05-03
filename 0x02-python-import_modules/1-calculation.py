255ffd52db54a132eef2dbc4b7fcd5d749367d97
ledbagholberton <789@holbertonschool.com>
2019-05-03 02:18:40 +0000

    fix format import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    c = calculator_1.add(a, b)
-    d = calculator_1.sub(a, b)
-    e = calculator_1.mul(a, b)
-    f = calculator_1.div(a, b)
+    c = add(a, b)
+    d = sub(a, b)
+    e = mul(a, b)
+    f = div(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, c))
     print("{:d} - {:d} = {:d}".format(a, b, d))
     print("{:d} * {:d} = {:d}".format(a, b, e))
