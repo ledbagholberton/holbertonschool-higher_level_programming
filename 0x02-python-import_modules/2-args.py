1d29f9ba85d9cc642ae113e67f29fbde4dbe77f9
ledbagholberton <789@holbertonschool.com>
2019-05-03 02:25:18 +0000

    Fix format import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) == 1:
-        print("{} arguments.".format(len(sys.argv) - 1))
+    from sys import argv
+    if len(argv) == 1:
+        print("{} arguments.".format(len(argv) - 1))
     else:
-        print("{} arguments.".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments.".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
