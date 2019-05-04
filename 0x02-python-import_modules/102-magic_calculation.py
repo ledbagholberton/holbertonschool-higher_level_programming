1a6a181ec0f4011176a7f32025df8697751ab8d4
ledbagholberton <789@holbertonschool.com>
2019-05-04 05:07:27 +0000

    Assembler

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    a = 10
-    b = 5
-    c = add(a, b)
-    d = sub(a, b)
-    e = mul(a, b)
-    f = div(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, c))
-    print("{:d} - {:d} = {:d}".format(a, b, d))
-    print("{:d} * {:d} = {:d}".format(a, b, e))
-    print("{:d} / {:d} = {:d}".format(a, b, f))
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if (a < b):
+        c = add(a,b)
+        for i in range (4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return sub(a,b)
