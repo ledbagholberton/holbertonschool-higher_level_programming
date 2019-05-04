875d56621d0c7f7b11c15396aff3f084ab5da052
Giovanny Perez <789@holbertonschool.com>
2019-05-04 04:22:53 +0000

    pruebas

--- 0x02-python-import_modules/prueba.py
+++ 0x02-python-import_modules/prueba.py
@@ -1,10 +1,19 @@
+
 #!/usr/bin/python3
+
 import dis
-def magic_calculation(a, b):
-    from magic_calculation_2 import add, sub
+from magic_calculation_102 import add, sub
+def magic_calculation_2(a, b):
 
+    a = 1
+    b = 5
 
+    if (a < b):
+        add(a,b)
 
-
-
-dis.dis(multi2)
+    for i in range (0, 90):
+        c = c + i
+        return (c)
+    else:
+        return sub(a,b)
+    dis.dis(magic_calculation)
