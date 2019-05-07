cf9cf33acff4683061c6d64701ccf7d84e143745
Giovanny Perez <789@holbertonschool.com>
2019-05-07 22:37:13 +0000

    Ahora si

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
     table = {67: None, 99: None}
-    return(my_string.translate(table))
+    return(my_string.translate({ord(i):None for i in "Cc"}))
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if (len(tuple_b) is 0):
-        tc0 = tuple_a[0]
-        tc1 = tuple_a[1]
-    elif(len(tuple_b) is 1):
-        tc0 = tuple_a[0] + tuple_b[0]
-        tc1 = tuple_a[1]
-    elif (len(tuple_a) is 0):
-        tc0 = tuple_b[0]
-        tc1 = tuple_b[1]
-    elif(len(tuple_a) is 1):
-        tc0 = tuple_b[0] + tuple_a[0]
-        tc1 = tuple_b[1]
-    else:
-        tc0 = tuple_b[0] + tuple_a[0]
-        tc1 = tuple_b[1] + tuple_a[1]
+        tb0 = 0
+        tb1 = 0
+    if(len(tuple_b) is 1):
+        tb1 = 0
+    if (len(tuple_a) is 0):
+        ta0 = 0
+        ta1 = 0
+    if(len(tuple_a) is 1):
+        ta1 = 0
+
+
     elif(len(tuple_a) is 2):
         tc0 = tuple_a[0] + tuple_b[0]
         tc1 = tuple_a[1] + tuple_b[1]
