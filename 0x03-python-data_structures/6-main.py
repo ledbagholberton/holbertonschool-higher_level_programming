4eccc79e1fb399909389c938008ef3754cc2345d
Giovanny Perez <789@holbertonschool.com>
2019-05-07 20:31:16 +0000

    Matrix

---
+++ 0x03-python-data_structures/6-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
+matrix = [
+    [1, 2, 3, 16],
+    [4, 5, 6, 16],
+    [7, 8, 9, 16],
+    [10, 11, 12, 16]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
