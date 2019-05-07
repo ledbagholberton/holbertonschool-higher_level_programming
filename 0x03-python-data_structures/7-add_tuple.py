3677a0bb478ac3d894576625db3ab782672044ef
Giovanny Perez <789@holbertonschool.com>
2019-05-07 22:53:46 +0000

    Changing tuplas to lists

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if (len(tuple_b) is 0):
-        tb0 = 0
-        tb1 = 0
-    if(len(tuple_b) is 1):
-        tb1 = 0
-    if (len(tuple_a) is 0):
-        ta0 = 0
-        ta1 = 0
-    if(len(tuple_a) is 1):
-        ta1 = 0
-
-
-    elif(len(tuple_a) is 2):
-        tc0 = tuple_a[0] + tuple_b[0]
-        tc1 = tuple_a[1] + tuple_b[1]
-
-    tuple_c = (tc0, tc1)
+    list_c = [0, 0]
+    list_a = list(tuple_a)
+    list_b = list(tuple_b)
+    if (len(list_b) is 0):
+        list_b = [0, 0]
+    if(len(list_b) is 1):
+        list_b.append(0)
+    if (len(list_a) is 0):
+        list_a = [0, 0]
+    if(len(list_a) is 1):
+        list_a.append(0)
+    list_c[0] = list_a[0] + list_b[0]
+    list_c[1] = list_a[1] + list_b[1]
+    
+    tuple_c = (list_c[0], list_c[1])
     return tuple_c
