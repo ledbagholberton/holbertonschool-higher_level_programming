e908be3b78c0d0e191947767adeee5d78929eaa6
Giovanny Perez <789@holbertonschool.com>
2019-05-07 17:42:38 +0000

    Case list empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     long = len(my_list) - 1
-    if (long + 1) > 0:
+    if (long + 1) >= 0:
         for i in range(long, -1, -1):
             print("{:d}".format(my_list[i]))
