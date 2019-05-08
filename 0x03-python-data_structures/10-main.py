715d8762fdbe70d486109e0de449a54caca28e67
Giovanny Perez <789@holbertonschool.com>
2019-05-08 00:00:42 +0000

    with correct shebang

--- 0x03-python-data_structures/10-main.py
+++ 0x03-python-data_structures/10-main.py
@@ -8,3 +8,19 @@ i = 0
 while i < len(list_result):
     print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
     i += 1
+
+my_list = [1, 3, 5, 7, 9, 11, 13]
+list_result = divisible_by_2(my_list)
+
+i = 0
+while i < len(list_result):
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1
+
+my_list = []
+list_result = divisible_by_2(my_list)
+
+i = 0
+while i < len(list_result):
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1
