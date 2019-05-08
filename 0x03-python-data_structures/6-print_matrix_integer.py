97a002a90bbd3c649910f8ff08c3b653a14964cd
Giovanny Perez <789@holbertonschool.com>
2019-05-08 04:55:34 +0000

    fix enter with matixx empty

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        print()
-    else:
+    if matrix is not None:
         long = len(matrix[0])
-        for i in range(long):
+        for i in range(0, long):
             long2 = len(matrix[i])
-            for j in range(long2 - 1):
+            for j in range(0, long2 - 1):
                 print("{} ".format(matrix[i][j]), end=" ")
             print("{} ".format(matrix[i][j + 1]))
+        else:
+            print()
+
