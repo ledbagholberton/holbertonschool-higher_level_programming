dc2197ad60c94af4af516ca082740db5f3aeb87b
Giovanny Perez <789@holbertonschool.com>
2019-05-08 03:37:57 +0000

    Fix dummy error in matix presentation

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     else:
         long = len(matrix[0])
         for i in range(long):
-           # long2 = len(matrix[i])
-           # for j in range(long2):
-                print(matrix[i])
+            long2 = len(matrix[i])
+            for j in range(long2 - 1):
+                print("{} ".format(matrix[i][j]), end=" ")
+            print("{} ".format(matrix[i][j + 1]))
