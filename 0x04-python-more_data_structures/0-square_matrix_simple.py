ba6f13b3b8a05b3b8b6127edc879c045d00cd2ab
Giovanny Perez <789@holbertonschool.com>
2019-05-10 03:21:58 +0000

    Fix with map & lambda & pep8

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix is not None:
-        if matrix ==  []:
+        if matrix == []:
             return matrix
         else:
             new_m = list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
