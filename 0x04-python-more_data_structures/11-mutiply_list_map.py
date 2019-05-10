dfe5c4247ad62f728b3317c56241209eb9278228
Giovanny Perez <789@holbertonschool.com>
2019-05-10 02:31:10 +0000

    Lambda & Map

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
     new_list = my_list.copy()
-    return list(map(lambda x: 2*x, new_list))
+    return list(map(lambda x: number*x, new_list))
