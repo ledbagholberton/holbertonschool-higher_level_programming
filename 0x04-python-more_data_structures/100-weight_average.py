a353be19d6e1c29ff558a8b5daac1c743ba7d5de
Giovanny Perez <789@holbertonschool.com>
2019-05-10 05:40:51 +0000

    First stike

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is []:
+        return 0
+    else:
+        for i in my_list:
+            print (my_list[i[1][0]])
+            p1 = a[0]
+            p2 = a[1]
+            print(a, p1, p2)
+    return (25)
