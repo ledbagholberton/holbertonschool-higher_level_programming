88f6a3cdcf38dfc254a05242c9ba06da14081e2b
Giovanny Perez <789@holbertonschool.com>
2019-05-10 02:53:33 +0000

    Issue matix origin

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            a = new_matrix[i][j]**2
-            new_matrix[i][j] = a
-    return(new_matrix)
+    if matrix is not None:
+        if matrix ==  []:
+            return matrix
+        else:
+            new_matrix = matrix.copy()
+            for i in range(0, len(matrix)):
+                for j in range(0, len(matrix[i])):
+                    a = new_matrix[i][j]**2
+                    new_matrix[i][j] = a
+            return(new_matrix)
