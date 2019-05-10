d1b29cce0a171cfc22d0ee6415f94a91f2eb1f8c
Giovanny Perez <789@holbertonschool.com>
2019-05-10 04:05:13 +0000

    Fix error check

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    mayor = 0
-    for a, b in a_dictionary.items():
-        if b > mayor:
-            mayor = b
-            key_mayor = a
-    if mayor == 0:
-        key_mayor = "None"
-    return key_mayor
+    if a_dictionary is not None:
+        if len(a_dictionary) is not 0:
+            mayor = 0
+            for a, b in a_dictionary.items():
+                if b > mayor:
+                    mayor = b
+                    key_mayor =  a
+            return key_mayor
