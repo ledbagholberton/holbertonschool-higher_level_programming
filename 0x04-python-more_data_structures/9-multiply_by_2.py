91edc3f44d44cf563197a1efadd001ebfc5d51bb
Giovanny Perez <789@holbertonschool.com>
2019-05-10 03:57:14 +0000

    Fix error check + sorted

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_d = a_dictionary.copy()
-    for n, v in new_d.items():
+    for n, v in sorted(new_d.items()):
         item_dict = {n: v * 2}
         new_d.update(item_dict)
     return(new_d)
