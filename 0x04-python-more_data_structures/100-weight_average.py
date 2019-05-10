31ac56185be95aec5aafb21dbd6d360671ffbcb4
Giovanny Perez <789@holbertonschool.com>
2019-05-10 05:52:42 +0000

    average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,7 +4,12 @@ def weight_average(my_list=[]):
         return 0
     else:
         a, b = 0, 0
+
         for (x, y) in my_list:
+            if x == None:
+                x = 0
+            if y == None:
+                y = 0
             a = (x * y) + a
             b = y + b
         r = a / b
