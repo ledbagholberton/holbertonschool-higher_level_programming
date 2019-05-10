1d0d430b9033d10237bc65db119129ccb8be6d65
Giovanny Perez <789@holbertonschool.com>
2019-05-10 03:31:20 +0000

    Fix error empty

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list is not None
-    suma = 0
-    for i in range(1, 10):
-        if i in my_list:
-            suma = suma + i
-    return(suma)
+    if my_list is not None:
+        suma = 0
+        for i in range(1, 10):
+            if i in my_list:
+                suma = suma + i
+        return(suma)
