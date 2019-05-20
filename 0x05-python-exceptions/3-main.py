70c02f74c3d3ba3a18ab630b0888c660943423b5
Giovanny Perez <789@holbertonschool.com>
2019-05-20 17:47:16 +0000

    Div by 0

--- 0x05-python-exceptions/3-main.py
+++ 0x05-python-exceptions/3-main.py
@@ -10,9 +10,3 @@ a = 12
 b = 0
 result = safe_print_division(a, b)
 print("{:d} / {:d} = {}".format(a, b, result))
-
-guillaume@ubuntu:~/0x05$ ./3-main.py
-Inside result: 6.0
-12 / 2 = 6.0
-Inside result: None
-12 / 0 = None
