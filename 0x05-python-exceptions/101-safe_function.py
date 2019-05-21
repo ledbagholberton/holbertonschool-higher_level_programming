61af3d454850206dc10a4a1d7fcdbc4f0ad495da
Giovanny Perez <789@holbertonschool.com>
2019-05-21 02:55:58 +0000

    Advanced 2

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,10 +2,9 @@
 import sys
 
 
-
 def safe_function(fct, *args):
     try:
-        return (fct(*args))
+        return(fct(*args))
     except(IndexError):
         print("Exception: list index out of range", file=sys.stderr)
     except (ZeroDivisionError):
