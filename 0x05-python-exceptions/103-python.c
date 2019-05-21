3f3fc5942a62c14a1c8f643fda2037f65159637c
Giovanny Perez <789@holbertonschool.com>
2019-05-21 05:05:53 +0000

    Fixing issues

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
- *
- *
+ *print_python_list_info - print list
+ *@p: PyObject
+ *Return:  No
  */
 
 void print_python_list_info(PyObject *p)
@@ -14,6 +15,7 @@ void print_python_list_info(PyObject *p)
 
 	tam_list = PyList_Size(p);
 	alloc_1 = ((PyListObject *)p)->allocated;
+	printf("[*] Python list info");
 	printf("[*] Size of the Python List = %li\n", tam_list);
 	printf("[*] Allocated = %li\n", alloc_1);
 	for (iter = 0; iter < tam_list; iter++)
@@ -25,50 +27,44 @@ void print_python_list_info(PyObject *p)
 }
 
 /**
- *
- *
+ *print_python_bytes - print bytes
+ *@p: PyObject
+ *Return:  No
  */
 void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t tam_list, iter, alloc_1;
-        PyObject *pyobject_per;
-        const char *type_obj;
+	PyObject *pyobject_per;
+	const char *type_obj;
 
-        tam_list = PyList_Size(p);
-        alloc_1 = ((PyListObject *)p)->allocated;
-        printf("[.] float object info\n");
-	printf("value: %f1.0\n", value);
+	size = PyBytes_Size(p);
+	string = PyBytes_AsString(p);
+	printf("[.] bytes object info\n");
+	printf("size: %d\n", size);
 	printf("trying string: %s\n", string);
-	printf("first %d bytes:");
-        for (iter = 0; iter < tam_list; iter++)
-        {
-                pyobject_per = PyList_GetItem(p, iter);
-                type_obj = Py_TYPE(pyobject_per)->tp_name;
-                printf("Element %li: %s\n", iter, type_obj);
-        }
+	if (size < 11)
+		tam = size;
+	else
+		tam = 10;
+	printf("first %d bytes:", tam);
+	for (iter = 0; iter < tam_list; iter++)
+	{
+		printf("%x ", string[iter]);
+	}
 }
-
 /**
- *
- *
+ *print_python_float - print float
+ *@p: PyObject
+ *Return:  No
  */
 void print_python_float(PyObject *p)
 {
 	Py_ssize_t tam_list, iter, alloc_1;
-        PyObject *pyobject_per;
-        const char *type_obj;
+	PyObject *pyobject_per;
+	const char *type_obj;
 
-        tam_list = PyList_Size(p);
-        alloc_1 = ((PyListObject *)p)->allocated;
-        printf("[*] Size of the Python List = %li\n", tam_list);
-        printf("[*] Allocated = %li\n", alloc_1);
-        for (iter = 0; iter < tam_list; iter++)
-
-
-        {
-                pyobject_per = PyList_GetItem(p, iter);
-                type_obj = Py_TYPE(pyobject_per)->tp_name;
-                printf("Element %li: %s\n", iter, type_obj);
-        }
-
+	tam_list = PyList_Size(p);
+	alloc_1 = ((PyListObject *)p)->allocated;
+	printf("[.] float object info\n");
+	printf("  value: %f1.0", value);
 }
