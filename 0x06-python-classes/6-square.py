9b8811c6b417cc060405a403a201f333d7cd420f
Giovanny Perez <789@holbertonschool.com>
2019-05-22 03:26:17 +0000

    Changing possition by value

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -27,28 +27,28 @@ class Square:
         return self.__position
 
     @position.setter
-    def position(self, position):
-        if type(position) is not tuple:
+    def position(self, value):
+        if type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif (len(position) > 2):
+        elif (len(value) > 2):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif (type(position[0]) is not int or type(position[1]) is not int):
+        elif (type(value[0]) is not int or type(value[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif position[0] < 0 or position[1] < 0:
+        elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = position
+            self.__position = value
 
     @property
     def size(self):
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if type(size) is not int:
+    def size(self, value):
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
             self.__area = self.__size * self.__size
