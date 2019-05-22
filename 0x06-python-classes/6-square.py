b081b76a58d4ac75652f5011371fa1679543702b
Giovanny Perez <789@holbertonschool.com>
2019-05-22 00:15:23 +0000

    Fix If Square = 0 condition

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,7 +30,7 @@ class Square:
     def position(self, position):
         if type(position) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif (len(position) >2):
+        elif (len(position) > 2):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif (type(position[0]) is not int or type(position[1]) is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
