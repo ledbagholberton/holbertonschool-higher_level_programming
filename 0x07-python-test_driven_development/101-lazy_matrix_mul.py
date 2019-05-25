3f7b30eae52ad6869adba6485eb402f44f9d1f87
Giovanny Perez <789@holbertonschool.com>
2019-05-25 04:50:44 +0000

    advanced

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import numpy as np
-def lazy_matrix_mul(m_a, m_b):
-    """ Function that multiplies two matrix with Numpy
+def matrix_mul(m_a, m_b):
+    """ Function that multiplies two matrix
 
         Args:
            m_a: matrix a
@@ -10,27 +10,32 @@ def lazy_matrix_mul(m_a, m_b):
         Returns:
            new matrix with the multiplication or Errors
     """
-    if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
+    if type(m_a) is not list:
+        raise TypeError("m_a must be a list")
+    if type(m_b) is not list:
+        raise TypeError("m_b must be a list")
     m_a_files = len(m_a)
     for row in m_a:
+        if type(row) is not list:
+            raise TypeError("m_a must be a list of lists")
         m_a_rows = len(row)
+        if m_a_rows == 0:
+            raise TypeError("m_a can't be empty")
+            for index, j in enumerate(row[:]):
+                if type(j) is not int and type(j) is not float:
+                    raise TypeError("m_a should contain only integers or floats")
+
     m_b_files = len(m_b)
     for row in m_b:
+        if type(row) is not list:
+            raise TypeError("m_b must be a list of lists")
         m_b_rows = len(row)
+        if m_b_rows == 0:
+            raise TypeError("m_b can't be empty")
+            for index, j in enumerate(row[:]):
+                if type(j) is not int and type(j) is not float:
+                    raise TypeError("m_b should contain only integers or floats")
+
     if (m_a_files is not m_b_rows):
         raise ValueError("m_a and m_b can't be multiplied")
-    new_matrix = []
-
-
-    for row_a in m_a:
-        for i, row_a in enumerate(row_a):
-
-        if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        for index, j in enumerate(row[:]):
-            if type(j) is not int and type(j) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            new_matrix[i][index] = round(j / div, 2)
-    return np.dot (m_a, m_b)
+    return np.dot(m_a, m_b)
