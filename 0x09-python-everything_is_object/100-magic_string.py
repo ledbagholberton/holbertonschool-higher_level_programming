ae53761376c4e41b485d69e2c96afbbb5fc0884f
Giovanny Perez <789@holbertonschool.com>
2019-05-29 00:39:51 +0000

    Holberton many times

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(self):
-    a = "Holberton"
-    return(a += [Holberton])
+def magic_string(cont=[0]):
+    cont[0] += 1
+    return("".join(list("Holberton" * cont[0])))
