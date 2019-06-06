c7dfe60a723071817bd6ff0b1d5fb8adf0a4bafd
Giovanny Perez <789@holbertonschool.com>
2019-06-06 04:38:38 +0000

    FIX issue enter at  the end file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as f:
-        a = f.read()
-        print("{:s}".format(a))
+        print(f.read(), end="")
