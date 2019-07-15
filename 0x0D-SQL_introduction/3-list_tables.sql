4d2e350d4aa224eaadcf4cf8ac121d38bf4a947f
Giovanny Perez <789@holbertonschool.com>
2019-07-15 20:47:32 +0000

    Tasks 1-5 MySQL

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- lists all the tables of a database in your MySQL server.
+-- command SHOW tables ... database name come in Command  line
+SHOW tables;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql~
@@ -0,0 +1,3 @@
+-- creates the database hbtn_0c_0 in your MySQL server.
+-- command CREATE DATABASE <name>
+DROP DATABASE IF EXISTS hbtn_0c_0;
