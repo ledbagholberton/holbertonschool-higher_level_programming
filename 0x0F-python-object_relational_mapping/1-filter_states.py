13e5d0a4b97f432b1e53254f25b5f5d268efebf3
ledbagholberton <789@holbertonschool.com>
2019-08-11 21:06:28 +0000

    FIX Order Asc

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' \
-    ORDER BY id DESC")
+    ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
