6e2ed2c36b71b39e8cb6fe55cd0b521edc83911b
Abdel <789@holbertonschool.com>
2019-08-20 22:30:59 +0000

    Fix w/o quotes

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Send a POST with variable
-curl -s --request POST $1 --data "email='hr@holbertonschool.com'&subject='I will always be here for PLD'"
+curl -s --request POST $1 --data "email=hr@holbertonschool.com&subject=I will always be here for PLD"
