0dc83219c1c5ec4737946d8d82b70e1b49e53da8
Abdel <789@holbertonschool.com>
2019-08-21 00:42:51 +0000

    WIth port and path

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Put a JSON file
-curl -sLX PUT -d "user_id=98" -H "origin:HolbertonSchool" 0.0.0.0:5000
+curl -sLX PUT -d "user_id=98" -H "origin:HolbertonSchool" 0.0.0.0:5000/catch_me
