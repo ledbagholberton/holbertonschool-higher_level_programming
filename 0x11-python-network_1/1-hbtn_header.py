a2f3329969c3496142d63bd44dbbf3640481b1d5
ledbagholberton <789@holbertonschool.com>
2019-08-26 22:04:58 +0000

    Fix tabs

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+"""request a specific header """
 import urllib.request
 import sys
 
 url = sys.argv[1]
 try:
-with urllib.request.urlopen(url) as response:
-    html = response.getheader("X-Request-Id")
-print(html)
+    with urllib.request.urlopen(url) as response:
+        html = response.getheader("X-Request-Id")
+    print(html)
 except:
     pass
