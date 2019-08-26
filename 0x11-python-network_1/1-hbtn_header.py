d6e2590514e121ca263488131491611da3aedc58
ledbagholberton <789@holbertonschool.com>
2019-08-26 22:01:01 +0000

    Fix Error in case URL doesnt exist

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,9 @@ import urllib.request
 import sys
 
 url = sys.argv[1]
+try:
 with urllib.request.urlopen(url) as response:
     html = response.getheader("X-Request-Id")
 print(html)
+except:
+    pass
