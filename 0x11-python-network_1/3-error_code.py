33384b2f52ad8083266fe1ac439779660e9449cd
ledbagholberton <789@holbertonschool.com>
2019-08-27 00:04:36 +0000

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,6 +7,7 @@ try:
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         html = response.read().decode("utf-8")
+        print(html)
 except urllib.error.URLError as e:
     if hasattr(e, 'code'):
         print ("Error code:", e.code)
