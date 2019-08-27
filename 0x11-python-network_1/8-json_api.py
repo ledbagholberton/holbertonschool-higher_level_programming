6c137d66e6ec4885774273d6fa90ce43a58fce76
ledbagholberton <789@holbertonschool.com>
2019-08-27 06:56:27 +0000

    Change in Exception

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,17 +3,18 @@
 import requests
 import sys
 
-url = "http://0.0.0.0:5000/search_user"
-if len(sys.argv) < 2:
-    data = {'q': ""}
-else:
-    data = {'q': sys.argv[1]}
-html = requests.post(url, data)
-try:
-    my_json = html.json()
-    if len(my_json) is 0:
-        print("No result")
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) < 2:
+        data = {'q': ""}
     else:
-        print("[{}] {}".format(my_json.get('id'), my_json.get('name')))
-except BaseException:
-    print("Not a valid JSON")
+        data = {'q': sys.argv[1]}
+    html = requests.post(url, data)
+    try:
+        my_json = html.json()
+        if len(my_json) is 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(my_json.get('id'), my_json.get('name')))
+    except BaseException:
+        print("Not a valid JSON")
