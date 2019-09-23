5e4e2d301afe50035480bb0c58315edf3144b5c0
ledbagholberton <789@holbertonschool.com>
2019-09-23 23:18:49 +0000

    Task 6 & 5 + semistandar2

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -12,4 +12,15 @@ module.exports = class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
+
+  double (w, h) {
+      this.width = 2 * this.width;
+      this.height = 2 * this.height;
+    }
+
+  rotate (w, h) {
+    const a = this.height;
+    this.height = this.width;
+    this.width = a;
+  }
 };
