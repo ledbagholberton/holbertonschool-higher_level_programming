2d249e72d4e21e3c8bdd895fd3a2726968f758d7
ledbagholberton <789@holbertonschool.com>
2019-09-24 04:27:47 +0000

    Fix order and no parameters

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,23 +2,23 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (h > 0 & w > 0) {
-      this.height = h;
       this.width = w;
+      this.height = h;
     }
   }
 
-  print (w, h) {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 
-  double (w, h) {
+  double () {
     this.width = 2 * this.width;
     this.height = 2 * this.height;
   }
 
-  rotate (w, h) {
+  rotate () {
     const a = this.height;
     this.height = this.width;
     this.width = a;
