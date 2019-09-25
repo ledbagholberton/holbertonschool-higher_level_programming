3987befac299ba7402d8c8049f07a6a54b368754
ledbagholberton <789@holbertonschool.com>
2019-09-25 01:02:36 +0000

    Format error print

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,6 +8,6 @@ try {
   const text = fs.readFileSync(path, 'utf8');
   console.log(text);
 } catch (err) {
-  const myDict = { 'Error: ': err.message, 'errno: ': err.errno, 'code: ': err.code, 'syscall: ': err.syscall, 'path: ': path };
-  console.log(myDict);
+//  const myDict = { 'Error: ': err.message, 'errno: ': err.errno, 'code: ': err.code, 'syscall: ': err.syscall, 'path: ': path };
+  console.log(err);
 }
