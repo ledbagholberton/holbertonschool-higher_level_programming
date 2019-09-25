5e3375d101f2d60952fc3016328f9887e2924316
ledbagholberton <789@holbertonschool.com>
2019-09-25 01:55:20 +0000

    Advanced 2

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv.slice(2);
+const url = 'http://swapi.co/api/films/' + args[0];
+const options = {
+  url: url,
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'Accept-Charset': 'utf-8'
+  }
+};
+
+request(options, (err, res, body) => {
+  if (err) { throw err; }
+  const myList = JSON.parse(body).characters;
+  for (let i = 0; i < myList.length; i++) {
+    request({ url: myList[i], method: 'GET' }, (err, res, body) => {
+      if (err) { console.log('Error'); }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
